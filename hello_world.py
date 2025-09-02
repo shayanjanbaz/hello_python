f454757e3989260171abe6e8f747c2153dfb1fb9
Shayan Janbaz <164884153+shayanjanbaz@users.noreply.github.com>
2025-09-02 09:02:09 +0330

    add file

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+
+makinmishi = 10
+print(makinmishi, "first value of makinmishi")
+makinmishi = 10000
+print(makinmishi, "second value of makinmishi")
+print("Salam maman!!!!!😘😘😘😘😘❤️❤️💕")
+print(f"{123} = {makinmishi} madar")
+print(123, "madar")
